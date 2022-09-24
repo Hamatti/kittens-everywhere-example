383809ecc4c1660dca42905933916f3c99f85bb6
Juha-Matti Santala <juhamattisantala@gmail.com>
2022-09-24 14:57:13 +0200

    Add 48x48 icon, prettify code

--- kittens_everywhere.js
+++ kittens_everywhere.js
@@ -2,8 +2,8 @@
  * Welcome to kittens-everywhere, an example extension
  * I (Juhis, hamatti.org) built for a class of "How to build
  * a web extension".
- * 
- * I've added a bunch of comments to explain what happens and 
+ *
+ * I've added a bunch of comments to explain what happens and
  * why.
  */
 
@@ -15,52 +15,52 @@ const MIN_IMAGE_DIMENSION = 150;
 
 /**
  * Our main function `kittenify` does the work in this content script
- * It loads the user preference set in an options page (see `options.html` 
+ * It loads the user preference set in an options page (see `options.html`
  * and `options.js`), then replaces each image with either a kitten or doggo.
  */
 function kittenify() {
-    /**
-     * The user preferences are stored in local storage.
-     * Note that this requires "permissions": ["storage"]
-     * in manifest.json so if you get a "browser.storage is undefined"
-     * error, check if it's missing there
-     */
-    let localStorage = browser.storage.local.get(['animalPreference'])
-    localStorage.then(function(res) {
-        // We default to kittens if the user has not set a preference
-        const animal = res.animalPreference || "kittens";
+  /**
+   * The user preferences are stored in local storage.
+   * Note that this requires "permissions": ["storage"]
+   * in manifest.json so if you get a "browser.storage is undefined"
+   * error, check if it's missing there
+   */
+  let localStorage = browser.storage.local.get(["animalPreference"]);
+  localStorage.then(function (res) {
+    // We default to kittens if the user has not set a preference
+    const animal = res.animalPreference || "kittens";
 
-        // We find all the images in the page
-        const images = document.querySelectorAll('img')
-        images.forEach(async function(img) {
-            // check if each image is large enough that we want to replace it
-            if (img.width > MIN_IMAGE_DIMENSION && img.height > MIN_IMAGE_DIMENSION) {
-                // and ask for the image data from the backend
-                const newImage = await browser.runtime.sendMessage({
-                    action: 'fetch',
-                    animal,
-                    size: {
-                        width: img.width, 
-                        height: img.height
-                    }
-                })
+    // We find all the images in the page
+    const images = document.querySelectorAll("img");
+    images.forEach(async function (img) {
+      // check if each image is large enough that we want to replace it
+      if (img.width > MIN_IMAGE_DIMENSION && img.height > MIN_IMAGE_DIMENSION) {
+        // and ask for the image data from the backend
+        const newImage = await browser.runtime.sendMessage({
+          action: "fetch",
+          animal,
+          size: {
+            width: img.width,
+            height: img.height,
+          },
+        });
 
-                /**
-                 * A nifty little trick:
-                 * we store the old image in the `img` element's
-                 * data-oldSrc data attribute so that reclicking
-                 * the extension icon brings back the old images
-                 */
-                if (img.dataset.oldSrc) {
-                    img.src = img.dataset.oldSrc
-                    delete img.dataset.oldSrc
-                } else {
-                    img.dataset.oldSrc = img.src
-                    img.src = newImage.imageUrl;
-                }
-            }
-        })
-    })
+        /**
+         * A nifty little trick:
+         * we store the old image in the `img` element's
+         * data-oldSrc data attribute so that reclicking
+         * the extension icon brings back the old images
+         */
+        if (img.dataset.oldSrc) {
+          img.src = img.dataset.oldSrc;
+          delete img.dataset.oldSrc;
+        } else {
+          img.dataset.oldSrc = img.src;
+          img.src = newImage.imageUrl;
+        }
+      }
+    });
+  });
 }
 
 /**
@@ -68,8 +68,8 @@ function kittenify() {
  * messages from the background script and run `kittenify` function
  * if we get a `kittenify` action.
  */
-browser.runtime.onMessage.addListener(function(message) {
-    if (message.action === 'kittenify') {
-        kittenify()
-    }
-})
+browser.runtime.onMessage.addListener(function (message) {
+  if (message.action === "kittenify") {
+    kittenify();
+  }
+});
